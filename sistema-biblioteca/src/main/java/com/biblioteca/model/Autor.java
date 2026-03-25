d4cdd8793431275239f13de7bdaf198524e2d41c
deividandrade <deividandrade.br@gmail.com>
2026-03-25 20:28:46 -0300

    feat: update Autor entity

--- sistema-biblioteca/src/main/java/com/biblioteca/model/Autor.java
+++ sistema-biblioteca/src/main/java/com/biblioteca/model/Autor.java
@@ -18,29 +18,29 @@ import java.util.List;
 @AllArgsConstructor
 public class Autor {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@NotBlank(message = "O nome do autor é obrigatório")
-	@Column(nullable = false)
-	private String nome;
+    @NotBlank(message = "O nome do autor é obrigatório")
+    @Column(nullable = false)
+    private String nome;
 
-	@Column(length = 500)
-	private String biografia;
+    @Column(length = 500)
+    private String biografia;
 
-	@Column
-	private String nacionalidade;
+    @Column
+    private String nacionalidade;
 
-	// Um autor pode ter vários livros
-	// mappedBy = nome do campo na classe Livro que referencia Autor
-	@OneToMany(mappedBy = "autor", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	private List<Livro> livros = new ArrayList<>();
+    // Um autor pode ter vários livros
+    // mappedBy = nome do campo na classe Livro que referencia Autor
+    @OneToMany(mappedBy = "autor", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Livro> livros = new ArrayList<>();
 
-	// Construtor sem a lista de livros (mais fácil de usar)
-	public Autor(String nome, String biografia, String nacionalidade) {
-		this.nome = nome;
-		this.biografia = biografia;
-		this.nacionalidade = nacionalidade;
-	}
+    // Construtor sem a lista de livros (mais fácil de usar)
+    public Autor(String nome, String biografia, String nacionalidade) {
+        this.nome = nome;
+        this.biografia = biografia;
+        this.nacionalidade = nacionalidade;
+    }
 }
